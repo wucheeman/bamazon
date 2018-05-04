9f3872c0d9c3119ba403340d642ea738b130556b
Mark Hainline <wucheeman@gmail.com>
2018-05-04 18:01:37 -0400

    pseudocode for bamazonCustomer.js

---
+++ bamazonCustomer.js
@@ -0,0 +1,13 @@
+// require for mysql and inquirer
+
+// connect to bamazon
+
+// retrieve all products
+
+// display product id, name, and price for each product
+
+// query id to purchase and quantity desired
+
+// check quantity available.
+
+// if quantity insufficient, refuse / else fulfill order, update quantity, and show total price
