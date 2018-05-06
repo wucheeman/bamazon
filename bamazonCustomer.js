9fbc99880bc00999fa92ef6959e4dc9ab7f0a1e4
Mark Hainline <wucheeman@gmail.com>
2018-05-06 15:00:26 -0400

    connect to bamazon DB and retrieve all product information

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,32 @@
 // require for mysql and inquirer
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 // connect to bamazon
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "mark",
+  password: "",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  getAllProducts();
+  connection.end();
+});
 
 // retrieve all products
+// TODO: refactor so this can be run from connect?
+function getAllProducts() {
+  connection.query("SELECT * FROM products", function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    //connection.end();
+  });
+}
 
 // display product id, name, and price for each product
 
