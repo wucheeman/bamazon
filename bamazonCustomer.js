7de2211756d6f8c2fcfe7125baece0a94387d8b9
Mark Hainline <wucheeman@gmail.com>
2018-05-07 11:04:56 -0400

    added prompt for product ID and quantity; check for quantity available; sells if enough and refuses if not

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,7 @@
 // =======================================================================
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+let productArray = [];
 
 // FUNCTIONS
 // ========================================================================
@@ -29,6 +30,46 @@ const computePad = (stringLength) => {
   return finalFormPad = stringifiedPad.toString().replace(/,/g, '');
 }
 
+const purchaseProduct = (numProducts) => {
+  inquirer
+  .prompt([
+    {
+      name: "productID",
+      type: "input",
+      message: "Input the ID of our fine product that you want ",
+      validate: function(value) {
+        if (isNaN(value) === false && value < numProducts) {
+          return true;
+        }
+        return false;
+      }
+    },
+    {
+      name: "quantity",
+      type: "input",
+      message: "How many ?" ,
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
+      }
+    }
+  ])
+  .then(function(answer) {
+     console.log(`You want to buy ${answer.quantity} unit(s) of ${answer.productID}`);
+     const index = answer.productID - 1;
+     const quantity = parseInt(answer.quantity);
+     if (quantity > productArray[index].stock_quantity) {
+      console.log(`Sorry, we don't have enough`);
+     } else {
+       // TODO compute price, etc
+       const total = quantity * productArray[index].price;
+       console.log(`Sold for $ ${total}.00!`);
+     }
+  }); // end of .then
+}
+
 // retrieve all products
 // TODO: refactor so this can be run from connect?
 function getAllProducts() {
@@ -39,6 +80,8 @@ function getAllProducts() {
     console.log('===================================================');
     console.log(' ID             PRODUCT NAME                PRICE')
     console.log('---------------------------------------------------');
+    productArray = result;
+    // TODO make these references to productArray
     for (var i = 0; i < result.length; i++) {
       let spacer = computeSpacer(i, result.length);
       let padNeeded = computePad(result[i].product_name.length);
@@ -49,6 +92,9 @@ function getAllProducts() {
                   ' $' + digitSpaceNeeded + result[i].price + '.00');
     }
     console.log('---------------------------------------------------');
+
+    // query id to purchase and quantity desired
+    purchaseProduct(productArray.length);
   });
 
 }
@@ -66,18 +112,18 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+  // console.log("connected as id " + connection.threadId);
+  // display product id, name, and price for each product
   getAllProducts();
+
+  // check quantity available.
+
+  // if quantity insufficient, refuse / else fulfill order, update quantity, and show total price
   connection.end();
 });
 
 
 
 
-// display product id, name, and price for each product
 
-// query id to purchase and quantity desired
 
-// check quantity available.
-
-// if quantity insufficient, refuse / else fulfill order, update quantity, and show total price
