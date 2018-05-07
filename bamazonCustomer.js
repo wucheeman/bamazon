b27499cbe866d3ce72ed8ce9de8afcd9d622628b
Mark Hainline <wucheeman@gmail.com>
2018-05-07 10:19:14 -0400

    additional formatting refinements to product display

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,45 +1,10 @@
-// require for mysql and inquirer
+// GLOBAL VARIABLES
+// =======================================================================
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
-// connect to bamazon
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "mark",
-  password: "",
-  database: "bamazon"
-});
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  getAllProducts();
-  connection.end();
-});
-
-// retrieve all products
-// TODO: refactor so this can be run from connect?
-function getAllProducts() {
-  connection.query("SELECT * FROM products", function(err, result) {
-    if (err) throw err;
-    // console.log(result);
-    console.log('\n                OUR PRODUCTS');
-    console.log('===================================================');
-    console.log(' ID             PRODUCT NAME                PRICE')
-    console.log('---------------------------------------------------');
-    for (var i = 0; i < result.length; i++) {
-      let spacer = computeSpacer(i, result.length);
-      let padNeeded = computePad(result[i].product_name.length);
-      let digitSpaceNeeded = computeDigitSpace(result[i].price);
-      console.log(spacer + 
-                  result[i].item_id + " | " + 
-                  result[i].product_name + padNeeded + " | " + 
-                  ' $' + digitSpaceNeeded + result[i].price + '.00');
-    }
-    console.log('---------------------------------------------------');
-  });
-}
+// FUNCTIONS
+// ========================================================================
 
 const computeSpacer = (counter, arrayLength) => {
   if (counter < arrayLength - 1) {
@@ -63,6 +28,48 @@ const computePad = (stringLength) => {
     }
   return finalFormPad = stringifiedPad.toString().replace(/,/g, '');
 }
+
+// retrieve all products
+// TODO: refactor so this can be run from connect?
+function getAllProducts() {
+  connection.query("SELECT * FROM products", function(err, result) {
+    if (err) throw err;
+    // console.log(result);
+    console.log('\n                OUR PRODUCTS');
+    console.log('===================================================');
+    console.log(' ID             PRODUCT NAME                PRICE')
+    console.log('---------------------------------------------------');
+    for (var i = 0; i < result.length; i++) {
+      let spacer = computeSpacer(i, result.length);
+      let padNeeded = computePad(result[i].product_name.length);
+      let digitSpaceNeeded = computeDigitSpace(result[i].price);
+      console.log(spacer + 
+                  result[i].item_id + " | " + 
+                  result[i].product_name + padNeeded + " | " + 
+                  ' $' + digitSpaceNeeded + result[i].price + '.00');
+    }
+    console.log('---------------------------------------------------');
+  });
+
+}
+// APPLICATION
+// ==================================================================
+
+// connect to bamazon
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "mark",
+  password: "",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  getAllProducts();
+  connection.end();
+});
 
 
 
