60fda81349eca74a702932b70b87f1ce9ce8310e
Mark Hainline <wucheeman@gmail.com>
2018-05-11 10:27:20 -0400

    aligned column width to match output column width

--- bamazonSeed.sql
+++ bamazonSeed.sql
@@ -6,8 +6,8 @@ USE bamazon;
 
 CREATE TABLE products (
 	item_id INT(10) AUTO_INCREMENT NOT NULL, 
-	product_name VARCHAR(50),
-	department_name VARCHAR(30),
+	product_name VARCHAR(35),
+	department_name VARCHAR(25),
 	price DECIMAL(7,2) NULL,
 	stock_quantity INT NULL,
 	PRIMARY KEY (item_id)
