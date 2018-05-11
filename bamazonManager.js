84f344ba71fe711099fba835bb9ea7c1afb97290
Mark Hainline <wucheeman@gmail.com>
2018-05-11 09:17:56 -0400

    clean up and delint; ready for test

--- bamazonManager.js
+++ bamazonManager.js
@@ -7,11 +7,11 @@ let productArray = [];
 
 // connect to bamazon
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: 'localhost',
   port: 3306,
-  user: "mark",
-  password: "",
-  database: "bamazon"
+  user: 'mark',
+  password: '',
+  database: 'bamazon'
 });
 
 
@@ -20,14 +20,14 @@ var connection = mysql.createConnection({
 
 const displayOptions = () => {
   console.log('In display options');
-  connection.query("SELECT * FROM products", function (err, result) {
+  connection.query('SELECT * FROM products', function (err, result) {
     if (err) throw err;
     productArray = result;
     inquirer
       .prompt([
         {
-          name: "choice",
-          type: "list",
+          name: 'choice',
+          type: 'list',
           message: 'Please make a choice',
           choices: ['View Products', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
         }
@@ -51,7 +51,7 @@ const displayOptions = () => {
         }
       }); // end of .then
   }); // end of connection.query
-} // end of displayOptions
+}; // end of displayOptions
 
 const viewProducts = () => {
   console.log('in viewProducts');
@@ -83,14 +83,14 @@ const computeSpacer = (counter, arrayLength) => {
     return '  '; // double space; 
   }
   return ' '; // single space;
-}
+};
 
 const computeDigitSpace = (quantity) => {
   if (quantity < 10) {
     return ' ';
   }
   return ''; // empty string
-}
+};
 
 const computePad = (stringLength) => {
   // TODO: (future) link pad length to field size
@@ -99,8 +99,8 @@ const computePad = (stringLength) => {
   for (let i = 0; i < padNeeded; i++) {
     stringifiedPad.push(' ');
   }
-  return finalFormPad = stringifiedPad.toString().replace(/,/g, '');
-}
+  return stringifiedPad.toString().replace(/,/g, '');
+};
 
 // END REMOVE
 
@@ -118,9 +118,9 @@ const viewLowInventory = () => {
         alignment: 'center',
       }
     }
-  }; 
+  };
   let titles = ['ITEM ID ', 'PRODUCT', 'IN STOCK'];
-  let data = productArray.filter(function(product) {
+  let data = productArray.filter(function (product) {
     return product.stock_quantity < 5;
   });
   let nameAndQuantity = data.map(function (product) {
@@ -134,138 +134,134 @@ const viewLowInventory = () => {
 const addToInventory = () => {
   console.log('in addToInventory');
   inquirer
-  .prompt([
-    {
-      name: "productID",
-      type: "input",
-      message: "\nInput the ID of the product ",
-      validate: function(value) {
-        if (value !== '' && 
-            isNaN(value) === false && 
-            value <= productArray.length && 
+    .prompt([
+      {
+        name: 'productID',
+        type: 'input',
+        message: '\nInput the ID of the product ',
+        validate: function (value) {
+          if (value !== '' &&
+            isNaN(value) === false &&
+            value <= productArray.length &&
             value > 0) {
-          return true;
+            return true;
+          }
+          return false;
         }
-        return false;
-      }
-    },
-    {
-      name: "quantity",
-      type: "input",
-      message: "How many units?" ,
-      validate: function(value) {
-        if (value !== '' && 
+      },
+      {
+        name: 'quantity',
+        type: 'input',
+        message: 'How many units?',
+        validate: function (value) {
+          if (value !== '' &&
             isNaN(value) === false &&
             value > -1) {
-          return true;
+            return true;
+          }
+          return false;
         }
-        return false;
       }
-    }
-  ])
-  .then(function(answer) {
-    const productID = answer.productID;
-    const index = parseInt(answer.productID) - 1;
-    const quantity = parseInt(answer.quantity) + productArray[index].stock_quantity;
-    //console.log(index, quantity);
-    if (quantity === 0) {
-      console.log('No update required');
-      connection.end();
-    } else {
-      // NOT DRY with bamazon.js
-      // TODO (future) modularize application so only one instance is needed
-      updateQuery = `
-        UPDATE products
-        SET stock_quantity = ${quantity}
-        WHERE item_id = ${productID};
-      `
-      // console.log(updateQuery);
-      connection.query(updateQuery, function(err, result) {
-        if (err) throw err;
-           connection.end();
-      }); // end of update query
-    } // else of else
-  }); // end of .then
-} // end of function
+    ])
+    .then(function (answer) {
+      const productID = answer.productID;
+      const index = parseInt(answer.productID) - 1;
+      const quantity = parseInt(answer.quantity) + productArray[index].stock_quantity;
+      //console.log(index, quantity);
+      if (quantity === 0) {
+        console.log('No update required');
+        connection.end();
+      } else {
+        // NOT DRY with bamazon.js
+        // TODO (future) modularize application so only one instance is needed
+        const updateQuery = `
+          UPDATE products
+          SET stock_quantity = ${quantity}
+          WHERE item_id = ${productID};
+        `;
+        // console.log(updateQuery);
+        connection.query(updateQuery, function (err, result) {
+          if (err) throw err;
+          connection.end();
+        }); // end of update query
+      } // else of else
+    }); // end of .then
+}; // end of function
 
 
 const addNewProduct = () => {
   console.log('in addNewProduct');
   inquirer
-  .prompt([
-    {
-      name: "product_name",
-      type: "input",
-      message: "\nName of the new product ",
-      validate: function(value) {
-        if (value !== '' ) {
-          return true;
+    .prompt([
+      {
+        name: 'product_name',
+        type: 'input',
+        message: '\nName of the new product ',
+        validate: function (value) {
+          if (value !== '') {
+            return true;
+          }
+          return false;
         }
-        return false;
-      }
-    },
-    {
-      name: "department_name",
-      type: "list",
-      message: "\nWhat department will sell it ",
-      choices: ['Gifts', 'Books', 'Clothing', 'Electronics', 'Musical Instruments', 'Outdoor Furnishings', 'Fine Art'],
-      validate: function(value) {
-        if (value !== '' && 
-            isNaN(value) === false && 
-            value <= productArray.length && 
+      },
+      {
+        name: 'department_name',
+        type: 'list',
+        message: '\nWhat department will sell it ',
+        choices: ['Gifts', 'Books', 'Clothing', 'Electronics', 'Musical Instruments', 'Outdoor Furnishings', 'Fine Art'],
+        validate: function (value) {
+          if (value !== '' &&
+            isNaN(value) === false &&
+            value <= productArray.length &&
             value > 0) {
-          return true;
+            return true;
+          }
+          return false;
         }
-        return false;
-      }
-    },
-    {
-      name: "price",
-      type: "input",
-      message: "\nWhat should its price be ",
-      validate: function(value) {
-        if (value !== '' && 
-            isNaN(value) === false &&  
-            value > 0) {
-          return true;
-        }
-        return false;
-      }
-    },
-    {
-      name: "quantity",
-      type: "input",
-      message: "\nHow many units?" ,
-      validate: function(value) {
-        if (value !== '' && 
+      },
+      {
+        name: 'price',
+        type: 'input',
+        message: '\nWhat should its price be ',
+        validate: function (value) {
+          if (value !== '' &&
             isNaN(value) === false &&
             value > 0) {
-          return true;
+            return true;
+          }
+          return false;
         }
-        return false;
+      },
+      {
+        name: 'quantity',
+        type: 'input',
+        message: '\nHow many units?',
+        validate: function (value) {
+          if (value !== '' &&
+            isNaN(value) === false &&
+            value > 0) {
+            return true;
+          }
+          return false;
+        }
       }
-    }
-  ])
-  .then(function(answer) {
-    let product_name = answer.product_name;
-    // console.log(typeof product_name);
-    let price = parseFloat(answer.price)
-    let stock_quantity = parseInt(answer.quantity);
-    // console.log(answer.product_name,
-    //             answer.department_name, 
-    //             price,
-    //             stock_quantity);
-    const newProdQuery = `
-      INSERT INTO products (product_name, department_name, price, stock_quantity)
-      VALUES('${product_name}', '${answer.department_name}', ${price}, ${stock_quantity});
-    `;
-    // console.log(newProdQuery);
-    connection.query(newProdQuery, function(err, result) {
-      if (err) throw err;
-      connection.end();
-    }); // end of new product query
-  }); // end of .then
-} // end of function
+    ])
+    .then(function (answer) {
+      let product_name = answer.product_name;
+      // console.log(typeof product_name);
+      let price = parseFloat(answer.price)
+      let stock_quantity = parseInt(answer.quantity);
+      const newProdQuery = `
+        INSERT INTO products (product_name, department_name, price, stock_quantity)
+        VALUES('${product_name}', '${answer.department_name}', ${price}, ${stock_quantity});
+      `;
+      // console.log(newProdQuery);
+      connection.query(newProdQuery, function (err, result) {
+        if (err) throw err;
+        connection.end();
+      }); // end of new product query
+    }); // end of .then
+}; // end of function
 
 // APPLICATION
 //==================================================================
